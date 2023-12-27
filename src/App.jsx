94a47530fe664ce200a0c6fc77b977629044b9a6
aamiryaqoob <aamiryaqoob965@gmail.com>
2023-12-28 02:04:54 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcept/CoreConcepts.jsx";
+import TabButtons from "./components/TabButtons.jsx";
 
 function App() {
+  function onSelectHandler(){
+    console.log("selected")
+}
   return (
     <div>
       <Header />
@@ -19,6 +23,16 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButtons onSelect={() => onSelectHandler("Components")}>Components</TabButtons>
+            <TabButtons onSelect={() => onSelectHandler("JSX")}>JSX</TabButtons>
+            <TabButtons onSelect={() => onSelectHandler("Props")}>props</TabButtons>
+            <TabButtons onSelect={() => onSelectHandler("State")}>state</TabButtons>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
